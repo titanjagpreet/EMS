eadb3b11cbe31245f4ffc04591d6e92ec2cb29a3
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-29 22:57:34 +0530

    Task List component

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,143 @@
+import React from 'react'
+
+export default function TaskList() {
+
+    return (
+        <>
+            <div id='tasklist' className='h-[55%] w-full overflow-x-auto flex items-center justify-start gap-3 flex-nowrap !mt-10 rounded-2xl !p-5 '>
+
+                <div className='h-full w-[300px] flex-shrink-0 bg-red-400 rounded-xl !p-5'>
+
+                    <div className='flex justify-between '>
+                        <h3 className='bg-red-600 text-sm !px-3 !py-1 rounded'>
+                            High
+                        </h3>
+
+                        <h2 className='text-white'>
+                            20 Feb 2025
+                        </h2>
+                    </div>
+
+                    <h2 className='!mt-6 text-2xl font-semibold text-white '>
+                        Make a YouTubeVideo
+                    </h2>
+
+                    <p className='text-sm text-white !mt-3 font-medium'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione error corporis dolore corrupti voluptatem dolorem reiciendis earum?
+                    </p>
+                </div>
+
+                <div className='h-full w-[300px] flex-shrink-0 bg-blue-400 rounded-xl !p-5'>
+
+                    <div className='flex justify-between '>
+                        <h3 className='bg-red-600 text-sm !px-3 !py-1 rounded'>
+                            High
+                        </h3>
+
+                        <h2 className='text-white'>
+                            20 Feb 2025
+                        </h2>
+                    </div>
+
+                    <h2 className='!mt-6 text-2xl font-semibold text-white '>
+                        Make a YouTubeVideo
+                    </h2>
+
+                    <p className='text-sm text-white !mt-3 font-medium'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione error corporis dolore corrupti voluptatem dolorem reiciendis earum?
+                    </p>
+                </div>
+
+
+                <div className='h-full w-[300px] flex-shrink-0 bg-[#C7734E] rounded-xl !p-5'>
+
+                    <div className='flex justify-between '>
+                        <h3 className='bg-red-600 text-sm !px-3 !py-1 rounded'>
+                            High
+                        </h3>
+
+                        <h2 className='text-white'>
+                            20 Feb 2025
+                        </h2>
+                    </div>
+
+                    <h2 className='!mt-6 text-2xl font-semibold text-white '>
+                        Make a YouTubeVideo
+                    </h2>
+
+                    <p className='text-sm text-white !mt-3 font-medium'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione error corporis dolore corrupti voluptatem dolorem reiciendis earum?
+                    </p>
+                </div>
+
+
+                <div className='h-full w-[300px] flex-shrink-0 bg-green-400 rounded-xl !p-5'>
+
+                    <div className='flex justify-between '>
+                        <h3 className='bg-red-600 text-sm !px-3 !py-1 rounded'>
+                            High
+                        </h3>
+
+                        <h2 className='text-white'>
+                            20 Feb 2025
+                        </h2>
+                    </div>
+
+                    <h2 className='!mt-6 text-2xl font-semibold text-white '>
+                        Make a YouTubeVideo
+                    </h2>
+
+                    <p className='text-sm text-white !mt-3 font-medium'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione error corporis dolore corrupti voluptatem dolorem reiciendis earum?
+                    </p>
+                </div>
+
+
+                <div className='h-full w-[300px] flex-shrink-0 bg-yellow-400 rounded-xl !p-5'>
+
+                    <div className='flex justify-between '>
+                        <h3 className='bg-red-600 text-sm !px-3 !py-1 rounded'>
+                            High
+                        </h3>
+
+                        <h2 className='text-white'>
+                            20 Feb 2025
+                        </h2>
+                    </div>
+
+                    <h2 className='!mt-6 text-2xl font-semibold text-white '>
+                        Make a YouTubeVideo
+                    </h2>
+
+                    <p className='text-sm text-white !mt-3 font-medium'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione error corporis dolore corrupti voluptatem dolorem reiciendis earum?
+                    </p>
+                </div>
+                
+
+                <div className='h-full w-[300px] flex-shrink-0 bg-[#A9C070] rounded-xl !p-5'>
+
+                    <div className='flex justify-between '>
+                        <h3 className='bg-red-600 text-sm !px-3 !py-1 rounded'>
+                            High
+                        </h3>
+
+                        <h2 className='text-white'>
+                            20 Feb 2025
+                        </h2>
+                    </div>
+
+                    <h2 className='!mt-6 text-2xl font-semibold text-white '>
+                        Make a YouTubeVideo
+                    </h2>
+
+                    <p className='text-sm text-white !mt-3 font-medium'>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione error corporis dolore corrupti voluptatem dolorem reiciendis earum?
+                    </p>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
