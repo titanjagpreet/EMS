fd9817547e4de66f9d4bf3b1f8754cade3220732
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-29 22:59:26 +0530

    Minor Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Login from './components/auth/login'
+import EmployeeDashboard from './components/dashboard/EmployeeDashboard'
+import AdminDashboard from './components/dashboard/AdminDashboard'
 
 export default function App() {
 
   return (
     <>
-      <div>
-        Hello World!
-      </div>
+      {/* <Login/> */}
+      {/* <EmployeeDashboard /> */}
+      <AdminDashboard/>
     </>
   )
 }
