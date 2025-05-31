94cd31928c01b11586946249cfec01a089b74680
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-31 23:30:53 +0530

    Passed employee and admin data to Dashboards

--- src/components/dashboard/AdminDashboard.jsx
+++ src/components/dashboard/AdminDashboard.jsx
@@ -3,14 +3,14 @@ import Header from '../others/Header'
 import CreateTask from '../others/CreateTask'
 import AllTask from '../others/AllTask'
 
-export default function AdminDashboard() {
+export default function AdminDashboard({data}) {
 
     return (
         <>
             <div className='!p-10 bg-[#111111] h-screen '>
-                <Header />
-                <CreateTask/>
-                <AllTask/>
+                <Header data={data} role='admin' />
+                <CreateTask data={data} />
+                <AllTask data={data} />
             </div>
         </>
     )
--- src/components/dashboard/EmployeeDashboard.jsx
+++ src/components/dashboard/EmployeeDashboard.jsx
@@ -3,14 +3,14 @@ import Header from '../others/Header'
 import TaskData from '../others/TaskData'
 import TaskList from '../TaskList/TaskList'
 
-export default function EmployeeDashboard() {
+export default function EmployeeDashboard({data}) {
 
     return (
         <>
             <div className='!p-10 bg-[#1C1C1C] h-screen '>
-                <Header/>
-                <TaskData/>
-                <TaskList/>
+                <Header data={data} role='employee' />
+                <TaskData data={data} />
+                <TaskList data={data} />
             </div>
         </>
     )
