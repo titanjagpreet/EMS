5bdebb9addb280a1be65f42da53b884e84c3d274
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-06-02 21:00:18 +0530

    Passed data as props to dashboards

--- src/components/dashboard/AdminDashboard.jsx
+++ src/components/dashboard/AdminDashboard.jsx
@@ -3,14 +3,14 @@ import Header from '../others/Header'
 import CreateTask from '../others/CreateTask'
 import AllTask from '../others/AllTask'
 
-export default function AdminDashboard({data}) {
+export default function AdminDashboard(props) {
 
     return (
         <>
             <div className='!px-10 !py-8 bg-[#111111] h-screen '>
-                <Header data={data} role='admin' />
-                <CreateTask data={data} />
-                <AllTask data={data} />
+                <Header changeUser={props.changeUser} data={props.data} role={props.role} />
+                <CreateTask data={props.data} />
+                <AllTask data={props.data} />
             </div>
         </>
     )
--- src/components/dashboard/EmployeeDashboard.jsx
+++ src/components/dashboard/EmployeeDashboard.jsx
@@ -3,14 +3,14 @@ import Header from '../others/Header'
 import TaskData from '../others/TaskData'
 import TaskList from '../TaskList/TaskList'
 
-export default function EmployeeDashboard({data}) {
+export default function EmployeeDashboard(props) {
 
     return (
         <>
             <div className='!px-10 !py-8 bg-[#1C1C1C] h-screen '>
-                <Header data={data} role='employee' />
-                <TaskData data={data} />
-                <TaskList data={data} />
+                <Header changeUser={props.changeUser} data={props.data} role={props.role}/>
+                <TaskData data={props.data} />
+                <TaskList data={props.data} />
             </div>
         </>
     )
